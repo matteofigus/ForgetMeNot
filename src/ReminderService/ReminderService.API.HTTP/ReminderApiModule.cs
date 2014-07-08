0e33c21dd42972fad967d368c57a4740cb4194e7
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-07-07 17:03:59 -0700

    sketched out nancy module for reminders API

---
+++ src/ReminderService/ReminderService.API.HTTP/ReminderApiModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nancy;
+using ReminderService.Router;
+
+namespace ReminderService.API.HTTP
+{
+	public class ReminderApiModule : NancyModule
+	{
+		private readonly IPublish _bus;
+
+		//todo: look at making the actions Async operations
+		public ReminderApiModule (IPublish bus) 
+			: base("/reminders")
+		{
+			_bus = bus;
+
+			Get ["/{reminderId}"] = parameters.reminderId => {
+				//handle getting a reminder with this ID...
+			};
+
+			Put["/"] = reminder => {
+				//handle scheduling
+			};
+
+			Delete["/{reminderId}"] = parameters.reminderId => {
+				//cancel a reminder
+			};
+		}
+	}
+}
+
---
+++ src/ReminderService/ReminderService.API.HTTP/RootModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy;
+
+namespace ReminderService.API.HTTP
+{
+	public class RootModule : NancyModule
+	{
+		public RootModule ()
+		{
+			Get ["/"] = _ => {
+				//do lbstatus in here
+			};
+		}
+	}
+}
+
