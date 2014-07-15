47f2c8866f0188f1200be35e42f59b075f4d611b
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-07-15 13:59:48 -0700

    small refactor for use of OT Logger

--- src/ReminderService/ReminderService.API.HTTP/BootStrapper.cs
+++ src/ReminderService/ReminderService.API.HTTP/BootStrapper.cs
@@ -8,16 +8,15 @@ namespace ReminderService.API.HTTP
 {
 	public class Bootstrapper : DefaultNancyBootstrapper
 	{
-		//hmmm, I dont want this to log an entry against the BootStrapper type. I want it to log
-		//against something like "ReminderService.API.HTTP.Request"
-		private static ILogger<Bootstrapper> Logger = new BasicLogger<Bootstrapper>();
-
 		protected override void ApplicationStartup (Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
 		{
 			//define a custom error handler that will log all errors that occur when invoking a route.
 			//keeps this noisy logging code (try-catch, etc...) out of the route handlers
+			//hmmm, I dont want this to log an entry against the BootStrapper type. I want it to log
+			//against something like "ReminderService.API.HTTP.Request"
 			pipelines.OnError += (ctx, e) => {
-				Logger.LogException(Level.Error, e, "Error processing request " + ctx.Request.Url);
+				ILogger logger = new BasicLogger<Bootstrapper>();
+				logger.LogException(Level.Error, e, "Error processing request " + ctx.Request.Url);
 
 				//do i need to return null here?
 				return null;
