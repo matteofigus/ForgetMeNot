d7adc2260e116982bfd57674483780d903419f7b
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-07-15 12:33:23 -0700

    done refactoring unit tests. removed old test file.

--- src/ReminderService/ReminderService.Core.Tests/ScheduleReminder/Publish_all_reminders_that_are_due_at_the_same_time.cs
+++ src/ReminderService/ReminderService.Core.Tests/ScheduleReminder/Publish_all_reminders_that_are_due_at_the_same_time.cs
@@ -9,7 +9,58 @@ namespace ReminderService.Core.Tests
 	[TestFixture ()]
 	public class Publish_all_reminders_that_are_due_at_the_same_time : Scheduler_Spec
 	{
+		[TestFixtureSetUp]
+		public void given_there_are_some_reminders_due_at_the_same_time()
+		{
+			SetNow (SystemTime.Now ());
+			GivenA (new SystemMessage.Start ());
 
+			GivenA (new ReminderMessages.ScheduledReminderHasBeenJournaled (
+				new ReminderMessages.ScheduleReminder (
+					"http://deliveryUrl/1",
+					"content/type",
+					Now,
+					new byte[0])));
+
+			GivenA (new ReminderMessages.ScheduledReminderHasBeenJournaled (
+				new ReminderMessages.ScheduleReminder (
+					"http://deliveryUrl/2",
+					"content/type",
+					Now,
+					new byte[0])));
+
+			GivenA (new ReminderMessages.ScheduledReminderHasBeenJournaled (
+				new ReminderMessages.ScheduleReminder (
+					"http://deliveryUrl/3",
+					"content/type",
+					Now.AddMilliseconds (25),
+					new byte[0])));
+
+			GivenA (new ReminderMessages.ScheduledReminderHasBeenJournaled (
+				new ReminderMessages.ScheduleReminder (
+					"http://deliveryUrl/4",
+					"content/type",
+					Now,
+					new byte[0])));
+
+			GivenA (new ReminderMessages.ScheduledReminderHasBeenJournaled (
+				new ReminderMessages.ScheduleReminder (
+					"http://deliveryUrl/3",
+					"content/type",
+					Now.AddMilliseconds (25),
+					new byte[0])));
+		}
+
+		[Test]
+		public void should_publish_all_reminders_that_are_due_at_the_same_time()
+		{
+			FireTimer ();
+			Assert.AreEqual (3, Received.Count);
+
+			AdvanceTimeBy (26);
+			FireTimer ();
+			Assert.AreEqual (5, Received.Count);
+		}
 	}
 }
 
--- src/ReminderService/ReminderService.Core.Tests/ScheduleReminder/Scheduler_Spec.cs
+++ src/ReminderService/ReminderService.Core.Tests/ScheduleReminder/Scheduler_Spec.cs
@@ -68,5 +68,26 @@ namespace ReminderService.Core.Tests
 			get { return _receivedMessages; }
 		}
 	}
+
+	public class TestTimer : ITimer
+	{
+		private Action _callback;
+
+		public void FiresIn (int milliseconds, Action callback)
+		{
+			_callback = callback;
+		}
+
+		public void Fire()
+		{
+			if (_callback != null)
+				_callback.Invoke ();
+		}
+
+		public void Dispose ()
+		{
+			//nothing to do...
+		}
+	}
 }
 
