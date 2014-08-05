121b39ce181c31032217723e05ade4bed6f5affd
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-08-05 14:15:38 -0700

    tidied up the code, removed obsolete code

--- src/ReminderService/ReminderService.Core/Startup/SystemStartManager.cs
+++ src/ReminderService/ReminderService.Core/Startup/SystemStartManager.cs
@@ -18,8 +18,6 @@ namespace ReminderService.Core.Startup
 		private readonly static ILog Logger = LogManager.GetLogger(typeof(SystemStartManager));
 		private readonly IBus _bus;
 		private readonly List<IReplayEvents> _replayers;
-		private readonly IReplayEvents _cancellationReplayer;
-		private readonly IReplayEvents _remindersReplayer;
 
 		public SystemStartManager (IBus bus, IEnumerable<IReplayEvents> replayers)
 		{
@@ -30,65 +28,20 @@ namespace ReminderService.Core.Startup
 			_replayers = new List<IReplayEvents> (replayers);
 		}
 
-		public SystemStartManager (IBus bus, IReplayEvents cancellationReplayer, IReplayEvents remindersReplayer)
-		{
-			Ensure.NotNull (bus, "bus");
-			Ensure.NotNull (cancellationReplayer, "cancellationReplayer");
-			Ensure.NotNull (remindersReplayer, "remindersReplayer");
-
-			_bus = bus;
-			_cancellationReplayer = cancellationReplayer;
-			_remindersReplayer = remindersReplayer;
-		}
-
 		public void Handle (SystemMessage.Start start)
-		{
-			RunStartTasks_MergeObservables2 (SystemTime.Now (), () => _bus.Publish (new SystemMessage.InitializationCompleted ()));
-		}
-
-		private void RunStartTasks_MergeObservables2(DateTime startFrom, Action onCompleted)
 		{
 			// Merge the observable sequences from all the replayers in to one stream
 			// Play that stream over the bus to initialize components
 			// When all observable sequences have completed, then we send a message indicating that init is completed => start normal operation
 			// If any of the child observables error, then the merged observable will error => we will not publish the init completed message => the system will not start
 			Observable
-				.Merge(_replayers.Select (r => r.Replay<IMessage> (startFrom)))
+				.Merge (_replayers.Select (r => r.Replay<IMessage> (SystemTime.Now ())))
 				.Subscribe (
-					Observer.Create<IMessage> (
-						(message) => _bus.Publish (message),
-						(error) => Logger.Error(error),
-						() => onCompleted() //actually we want to publish a message indicating that all components have initialized
-					));
-		}
-
-		private void RunStartTasks_AsyncTasks()
-		{
-			var replayFrom = SystemTime.Now ();
-			var t1 = Task.Factory.StartNew (() => StartScheduler(replayFrom));
-			var t2 = Task.Factory.StartNew ( () => StartCancellationFilter(replayFrom));
-
-			Task.WaitAll (new []{ t1, t2 });
-		}
-
-		private void StartScheduler(DateTime startFrom)
-		{
-			//replay shceduler messages
-			//await until we have played all the messages
-			var observable = _remindersReplayer.Replay<ReminderMessage.Schedule> (startFrom);
-			var observer = Observer.Create<ReminderMessage.Schedule> (
-				(reminder) => _bus.Publish (reminder));
-			observable.Subscribe (observer);
-		}
-
-		private void StartCancellationFilter(DateTime startFrom)
-		{
-			//replay cancellation messages
-			//await until we have played all the cancellation messages
-			var observable = _cancellationReplayer.Replay<ReminderMessage.Cancel> (startFrom);
-			var observer = Observer.Create<ReminderMessage.Cancel> (
-				(cancel) => _bus.Publish (cancel));
-			observable.Subscribe (observer);
+				Observer.Create<IMessage> (
+					(message) => _bus.Publish (message), 								// OnNext
+					(error) => Logger.Error (error),									//OnError
+					() => _bus.Publish (new SystemMessage.InitializationCompleted ())	//OnCompleted
+				));
 		}
 	}
 }
