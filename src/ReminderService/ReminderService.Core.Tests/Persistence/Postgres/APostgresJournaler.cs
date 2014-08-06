26f723731ebb213ccc4dad3479f6fec2c723d248
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-08-06 10:58:42 -0700

    added unit test class for Postgres Journaler

---
+++ src/ReminderService/ReminderService.Core.Tests/Persistence/Postgres/APostgresJournaler.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ReminderService.Core.Tests
+{
+	[TestFixture ()]
+	public class APostgresJournaler
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
