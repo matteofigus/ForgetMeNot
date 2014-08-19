7f92690b2903f96977514cdad6fff639d6454d3d
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-08-19 14:55:02 -0700

    created a static class for holding query messages

---
+++ src/ReminderService/ReminderService.API.HTTP.Tests/When_getting_a_reminder.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ReminderService.API.HTTP.Tests
+{
+	[TestFixture ()]
+	public class When_getting_a_reminder
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
