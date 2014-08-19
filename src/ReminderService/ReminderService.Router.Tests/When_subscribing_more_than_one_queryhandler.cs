7f9c8ffdb7ff8436afbce36b0545e3cff1b5f171
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-08-19 12:44:05 -0700

    removed static references to Rx and replaced with Nuget Package for Rx. Everything now compiles and all tests pass, again, at last

---
+++ src/ReminderService/ReminderService.Router.Tests/When_subscribing_more_than_one_queryhandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ReminderService.Router.Tests.Helpers;
+
+namespace ReminderService.Router.Tests
+{
+    [TestFixture]
+    public class When_subscribing_more_than_one_queryhandler : Given_a_bus_instance
+    {
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Initialize_bus()
+        {
+            WithQueryHandler(new FakeQueryHandler<TestRequest, TestResponse>(r => new TestResponse()));
+            WithQueryHandler(new FakeQueryHandler<TestRequest, TestResponse>(r => new TestResponse()));
+        }
+    }
+}
