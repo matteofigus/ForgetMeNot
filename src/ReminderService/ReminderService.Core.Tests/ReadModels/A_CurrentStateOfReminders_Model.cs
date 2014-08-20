8d97d5d0b78ec665e1800126931c30efaf4711ea
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-08-20 11:56:52 -0700

    unit tests for the CurrentReminderState readmodel

---
+++ src/ReminderService/ReminderService.Core.Tests/ReadModels/A_CurrentStateOfReminders_Model.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System;
+using ReminderService.Core.ReadModels;
+using ReminderService.Messages;
+using ReminderService.Test.Common;
+using System.Collections.Generic;
+using ReminderService.Common;
+
+namespace ReminderService.Core.Tests.ReadModels
+{
+	[TestFixture]
+	public class A_CurrentStateOfReminders_Model
+	{
+		private CurrentStateOfReminders _reminderStates;
+		private List<ReminderMessage.Schedule> _reminders;
+
+		[SetUp]
+		public void Given_a_bunch_of_remimders()
+		{
+			_reminderStates = new CurrentStateOfReminders ();
+			_reminders = new List<ReminderMessage.Schedule> (MessageBuilders.BuildReminders (5));
+			foreach (var reminder in _reminders) {
+				_reminderStates.Handle (
+					new JournaledEnvelope<ReminderMessage.Schedule>(reminder)
+				);
+			}
+		}
+
+		[Test]
+		public void Should_keep_track_of_reminders ()
+		{
+			var reminderId = _reminders[2].ReminderId;
+			var query = new RequestResponse.GetReminderState (reminderId);
+			var response = _reminderStates.Handle (query);
+
+			Assert.IsTrue (response.HasValue);
+			Assert.AreEqual (reminderId, response.Value.Reminder.ReminderId);
+			Assert.AreEqual (RequestResponse.ReminderStatusEnum.Scheduled, response.Value.Status);
+		}
+
+		[Test]
+		public void Should_keep_track_of_cancellations ()
+		{
+			var reminderId = _reminders[1].ReminderId;
+			_reminderStates.Handle (new JournaledEnvelope<ReminderMessage.Cancel> (
+				new ReminderMessage.Cancel (reminderId))
+			);
+
+			var query = new RequestResponse.GetReminderState (reminderId);
+			var response = _reminderStates.Handle (query);
+
+			Assert.IsTrue (response.HasValue);
+			Assert.AreEqual (reminderId, response.Value.Reminder.ReminderId);
+			Assert.AreEqual (RequestResponse.ReminderStatusEnum.Canceled, response.Value.Status);
+		}
+
+		[Test]
+		public void Should_keep_track_of_delivered_reminders ()
+		{
+			var reminderId = _reminders[4].ReminderId;
+			_reminderStates.Handle (new ReminderMessage.Sent(reminderId, SystemTime.UtcNow()));
+
+			var query = new RequestResponse.GetReminderState (reminderId);
+			var response = _reminderStates.Handle (query);
+
+			Assert.IsTrue (response.HasValue);
+			Assert.AreEqual (reminderId, response.Value.Reminder.ReminderId);
+			Assert.AreEqual (RequestResponse.ReminderStatusEnum.Delivered, response.Value.Status);
+		}
+
+		[Test]
+		[Ignore]
+		public void Should_keep_track_of_undeliverable_reminders ()
+		{
+			var reminderId = _reminders[4].ReminderId;
+			_reminderStates.Handle (new ReminderMessage.Sent(reminderId, SystemTime.UtcNow()));
+
+			var query = new RequestResponse.GetReminderState (reminderId);
+			var response = _reminderStates.Handle (query);
+
+			Assert.IsTrue (response.HasValue);
+			Assert.AreEqual (reminderId, response.Value.Reminder.ReminderId);
+			Assert.AreEqual (RequestResponse.ReminderStatusEnum.Delivered, response.Value.Status);
+		}
+	}
+}
+
