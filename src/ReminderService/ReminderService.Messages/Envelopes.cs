ad507e307b33b5a7b9b13b0e20cd5c4a7a0fe6a9
Robert Macdonald Smith <rmacdonaldsmith@yahoo.co.uk>
2014-08-21 14:24:45 -0700

    removed DeliveryEnvelope - not used. Created general Envelope class.

--- src/ReminderService/ReminderService.Messages/Envelopes.cs
+++ src/ReminderService/ReminderService.Messages/Envelopes.cs
@@ -1,9 +1,10 @@
-﻿using ReminderService.Common;
+﻿using System;
 using ReminderService.Router.MessageInterfaces;
+using ReminderService.Common;
 
 namespace ReminderService.Messages
 {
-	public static class DeliveryEnvelope
+	public static class Envelopes
 	{
 		public class Http<T> : IMessage where T : class, IMessage
 		{
@@ -24,6 +25,22 @@ namespace ReminderService.Messages
 			{
 				Ensure.NotNull(toBeSent, "toBeSent");
 				Reminder = toBeSent;
+			}
+		}
+
+		public class JournaledEnvelope<T> : IMessage where T : class, IMessage
+		{
+			private readonly T _inner;
+
+			public T Message {
+				get {return _inner;}
+			}
+
+			public JournaledEnvelope (T journaledMessage)
+			{
+				Ensure.NotNull (journaledMessage, "innerMessage");
+
+				_inner = journaledMessage;
 			}
 		}
 	}
